50f6f0b0201f25c93076d2d53bb5dcbdb411adb7
Sherin24 <sherinsarah.varghese@my.jcu.edu.au>
2018-09-05 18:10:06 +0800

    Exercises of week 6

---
+++ exercises_week06/src/Couplet.java
@@ -0,0 +1,10 @@
+public class Couplet extends Poem
+{
+    private String title;
+    private int lines;
+
+    public Couplet(String name)
+    {
+        super(name, 2);
+    }
+}
---
+++ exercises_week06/src/Poem.java
@@ -0,0 +1,20 @@
+public class Poem
+{
+    private String title;
+    private int lines;
+
+    public Poem(String name, int numLines)
+    {
+        title = name;
+        lines = numLines;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+    public int getLines()
+    {
+        return lines;
+    }
+}
